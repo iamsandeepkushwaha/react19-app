7974c488643ef563ba0edae189fb330cb6d95824
Sandeep Kushwaha <shrisandeepkushwaha@gmail.com>
2026-02-15 11:42:43 +0530

    PHASE 1: React Foundations - (DAY 1 - React  Basics )

---
+++ react19-app/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+function Welcome() {
+    const name = "Sandeep";
+
+    return <div>
+        <h2>Hello {name}</h2>
+        <button>Click Me</button>
+        </div>;
+}
+
+export default Welcome;
