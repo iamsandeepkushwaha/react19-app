d53ddcd7dd7b99db427ae88dff5ad17f2dfd5c27
Sandeep Kushwaha <shrisandeepkushwaha@gmail.com>
2026-03-05 12:33:43 +0530

    PHASE 3: Advanced React - DAY 12 – Performance, Lazy Loading & Suspense

--- react19-app/src/App.jsx
+++ react19-app/src/App.jsx
@@ -1,15 +1,36 @@
-import ContactForm from "./forms/ContactForm";
-import Signup from "./forms/Signup";
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+// import Dashboard from "./pages/Dashboard";
+// Normal Import
 
-function App() {  
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+// Lazy Import - 👉 Dashboard tabhi load hoga jab use karega
 
-  return (<>
-  <ContactForm/>
-  <br/>
-  <h1>Signup Form</h1>
-  <Signup />
-  </>)
+function App() {
+  return (
+    <>
+      {/* Direct Lazy Loading Example */}
+      <Suspense fallback={<h2>Loading Dashboard...</h2>}>
+        <Dashboard />
+      </Suspense>
+      {/* 👉 Loading UI automatic 😍 */}
+
+      {/* Lazy Loading with Routing 🔥 (MOST COMMON) */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/dashboard"
+          element={
+            <Suspense fallback={<h2>Loading...</h2>}>
+              <Dashboard />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
-
 
 export default App;
